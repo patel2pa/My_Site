ccf08219f1bc23aa539f76240676d2bc0328b324
parth <parthpp1996@gmail.com>
2020-07-31 22:46:59 -0400

    added more to the timeline

--- src/Components/components2.0/timeLine.js
+++ src/Components/components2.0/timeLine.js
@@ -8,38 +8,36 @@ class TimeLine extends React.Component{
         <VerticalTimeline>
             <VerticalTimelineElement
                 className="vertical-timeline-element--work"
-                contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-                date="2011 - present"
-                iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
-                
-            >
-            <h3 className="vertical-timeline-element-title">Creative Director</h3>
-            <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-            <p>
-            Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-            </p>
-        </VerticalTimelineElement>
-            <VerticalTimelineElement
-                className="vertical-timeline-element--work"
-                date="2010 - 2011"
+                date="May 2020"
                 iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
             >
-            <h3 className="vertical-timeline-element-title">Art Director</h3>
-            <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+            <h3 className="vertical-timeline-element-title">JMU Engineering</h3>
+            <h4 className="vertical-timeline-element-subtitle">Harrisonburg, VA</h4>
             <p>
-            Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+            Graduated with a Bachelor of Science in Engineering
             </p>
         </VerticalTimelineElement>
             <VerticalTimelineElement
                 className="vertical-timeline-element--work"
-                date="2008 - 2010"
+                date="May 2020"
+                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            >
+            <h3 className="vertical-timeline-element-title">Hacking 4 Defense</h3>
+            <h4 className="vertical-timeline-element-subtitle">Harrisonburg, VA</h4>
+            <p>
+            Presented final solution to Military personals, experts, professors and peers
+            </p>
+        </VerticalTimelineElement>
+            <VerticalTimelineElement
+                className="vertical-timeline-element--work"
+                date="April 2020"
                 iconStyle={{ background: 'rgb(255, 0, 0)', color: '#fff' }}
             >
-            <h3 className="vertical-timeline-element-title">Web Designer</h3>
-            <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
+            <h3 className="vertical-timeline-element-title">Bearables</h3>
+            <h4 className="vertical-timeline-element-subtitle">Harrisonburg, VA</h4>
             <p>
-            User Experience, Visual Design
+            Finalized a system for remote communication of bear behavior data and 
+            IoT inferstructure     
             </p>
         </VerticalTimelineElement>
             <VerticalTimelineElement
